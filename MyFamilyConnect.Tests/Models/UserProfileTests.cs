1218e366adc8c0acd2b8c2685afac7e54f75dfb7
Matt Smart <mdsmart36@gmail.com>
2015-11-22 07:28:09 -0600

    added UserProfile model and UserProfileTests class

---
+++ MyFamilyConnect.Tests/Models/UserProfileTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyFamilyConnect.Tests.Models
+{
+    [TestClass]
+    public class UserProfileTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MyFamilyConnect/Models/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFamilyConnect.Models
+{
+    public class UserProfile
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone1 { get; set; }
+        public string Phone2 { get; set; }
+        public string Email { get; set; }
+        public string AboutMe { get; set; }
+                
+    }
+}
