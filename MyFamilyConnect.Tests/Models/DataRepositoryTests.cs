0805ff15c31f4c6a714aa660885d9bb3078ae29b
Matt Smart <mdsmart36@gmail.com>
2015-11-27 09:58:22 -0600

    finished CRUD operations on NewsPhotoItems

--- MyFamilyConnect.Tests/Models/DataRepositoryTests.cs
+++ MyFamilyConnect.Tests/Models/DataRepositoryTests.cs
@@ -27,6 +27,10 @@ namespace MyFamilyConnect.Tests.Models
             mock_NewsPhotos.As<IQueryable<NewsPhotoItem>>().Setup(m => m.Expression).Returns(data.Expression);
 
             mock_context.Setup(m => m.NewsAndPhotos).Returns(mock_NewsPhotos.Object);
+            // This allows BoardRepository to call Boards.Add and have it update the my_list instance and Enumerator
+            // Connect DbSet.Add to List.Add so they work together
+            mock_NewsPhotos.Setup(m => m.Add(It.IsAny<NewsPhotoItem>())).Callback((NewsPhotoItem b) => my_list.Add(b));
+            mock_NewsPhotos.Setup(m => m.Remove(It.IsAny<NewsPhotoItem>())).Callback((NewsPhotoItem b) => my_list.Remove(b));
         }
 
         [TestInitialize]
@@ -38,7 +42,6 @@ namespace MyFamilyConnect.Tests.Models
             user1 = new ApplicationUser();
             user2 = new ApplicationUser();
             user3 = new ApplicationUser();
-
         }
 
         [TestCleanup]
@@ -54,6 +57,192 @@ namespace MyFamilyConnect.Tests.Models
         {
             DataRepository data_repo = new DataRepository(mock_context.Object);
             Assert.IsNotNull(data_repo);
+        }
+
+        [TestMethod]
+        public void DataRepoCanCreateNewsPhotoItem()
+        {
+            // Arrange
+            DataRepository data_repo = new DataRepository(mock_context.Object);
+            NewsPhotoItem news_item1 = new NewsPhotoItem()
+            {
+                Title = "title",
+                HasPhoto = false,
+                Comments = null,
+                NewsPhotoId = 2,
+                UserProfileId = 1,
+                Text = "news text"
+            };
+            NewsPhotoItem news_item2 = new NewsPhotoItem()
+            {
+                Title = "title",
+                HasPhoto = false,
+                Comments = null,
+                NewsPhotoId = 2,
+                UserProfileId = 1,
+                Text = "news text"
+            };
+
+            ConnectMocksToDataSource();
+
+            // Act                        
+            bool actual = data_repo.AddNewsPhotoItem(news_item1);
+            // Assert
+            Assert.AreEqual(1, data_repo.GetAllNewsPhotosCount());
+            Assert.IsTrue(actual);
+            // Act
+            actual = false;            
+            actual = data_repo.AddNewsPhotoItem(news_item2);
+            // Assert
+            Assert.AreEqual(2, data_repo.GetAllNewsPhotosCount());
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void DataRepoCanGetAllNewsPhotoItems()
+        {
+            // Arrange
+            DataRepository data_repo = new DataRepository(mock_context.Object);
+            NewsPhotoItem news_item1 = new NewsPhotoItem()
+            {
+                Title = "title 1",
+                HasPhoto = false,
+                Comments = null,
+                NewsPhotoId = 1,
+                UserProfileId = 1,
+                Text = "news text 1"
+            };
+            NewsPhotoItem news_item2 = new NewsPhotoItem()
+            {
+                Title = "title 2",
+                HasPhoto = false,
+                Comments = null,
+                NewsPhotoId = 2,
+                UserProfileId = 1,
+                Text = "news text 2"
+            };
+
+            ConnectMocksToDataSource();
+
+            // Act           
+            data_repo.AddNewsPhotoItem(news_item1);
+            data_repo.AddNewsPhotoItem(news_item2);
+            int expected = 2;
+            int actual = data_repo.GetAllNewsPhotoItems().Count();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DataRepoEnsureZeroNewsPhotoItems()
+        {
+            // Arrange            
+            ConnectMocksToDataSource();
+            DataRepository data_repo = new DataRepository(mock_context.Object);
+
+            // Act
+            int expected = 0;
+            int actual = data_repo.GetAllNewsPhotosCount();
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }       
+
+        [TestMethod]
+        public void DataRepoCanGetNewsPhotoItemForUser()
+        {
+            // Arrange
+            DataRepository data_repo = new DataRepository(mock_context.Object);
+            NewsPhotoItem news_item1 = new NewsPhotoItem()
+            {
+                Title = "title 1",
+                HasPhoto = false,
+                Comments = null,
+                NewsPhotoId = 1,
+                UserProfileId = 1,
+                Text = "news text 1"
+            };
+            NewsPhotoItem news_item2 = new NewsPhotoItem()
+            {
+                Title = "title 2",
+                HasPhoto = false,
+                Comments = null,
+                NewsPhotoId = 2,
+                UserProfileId = 2,
+                Text = "news text 2"
+            };
+
+            ConnectMocksToDataSource();
+
+            // Act
+            data_repo.AddNewsPhotoItem(news_item1);
+            data_repo.AddNewsPhotoItem(news_item2);
+            int expected = 1;
+            int actual = data_repo.GetNewsPhotosForUser(news_item2.UserProfileId).Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DataRepoCanUpdateNewsPhotoItemTitle()
+        {
+            // Arrange
+            DataRepository data_repo = new DataRepository(mock_context.Object);
+            NewsPhotoItem news_item1 = new NewsPhotoItem()
+            {
+                Title = "title 1",
+                HasPhoto = false,
+                Comments = null,
+                NewsPhotoId = 1,
+                UserProfileId = 1,
+                Text = "news text 1"
+            };
+            ConnectMocksToDataSource();
+            data_repo.AddNewsPhotoItem(news_item1);
+
+            // Act
+            string expected = "new title";
+            var actual = data_repo.UpdateNewsPhotoTitle(news_item1.UserProfileId, news_item1.Title, expected);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void DataRepoCanDeleteNewsPhotoItem()
+        {
+            DataRepository data_repo = new DataRepository(mock_context.Object);
+            NewsPhotoItem news_item1 = new NewsPhotoItem()
+            {
+                Title = "title",
+                HasPhoto = false,
+                Comments = null,
+                NewsPhotoId = 2,
+                UserProfileId = 1,
+                Text = "news text"
+            };
+            NewsPhotoItem news_item2 = new NewsPhotoItem()
+            {
+                Title = "title",
+                HasPhoto = false,
+                Comments = null,
+                NewsPhotoId = 2,
+                UserProfileId = 1,
+                Text = "news text"
+            };
+
+            ConnectMocksToDataSource();
+
+            // Act                        
+            data_repo.AddNewsPhotoItem(news_item1);
+            data_repo.AddNewsPhotoItem(news_item2);
+            NewsPhotoItem news = data_repo.DeleteNewsPhotoItem(news_item1);
+
+            // Assert
+            Assert.IsNotNull(news);
+            Assert.AreEqual(news.UserProfileId, news_item1.UserProfileId);
+            Assert.AreEqual(1, data_repo.GetAllNewsPhotosCount());
         }
     }
 }
--- MyFamilyConnect/Models/DataRepository.cs
+++ MyFamilyConnect/Models/DataRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace MyFamilyConnect.Models
 {
@@ -12,6 +13,88 @@ namespace MyFamilyConnect.Models
         public DataRepository(DataContext _context)
         {
             context = _context;
+        }
+
+        public int GetAllNewsPhotosCount()
+        {            
+            return GetAllNewsPhotoItems().Count;
+        }
+
+        public bool AddNewsPhotoItem(NewsPhotoItem news_item)
+        {
+            bool result = true;
+            try
+            {
+                context.NewsAndPhotos.Add(news_item);
+                context.SaveChanges();                
+            }
+            catch (ArgumentNullException)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public List<NewsPhotoItem> GetAllNewsPhotoItems()
+        {
+            try
+            {
+                var query = from l in context.NewsAndPhotos select l;
+                return query.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }                        
+        }
+
+        public List<NewsPhotoItem> GetNewsPhotosForUser(int userProfileId)
+        {
+            try
+            {
+                var query = from l in context.NewsAndPhotos where l.UserProfileId == userProfileId select l;
+                return query.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+
+        public bool UpdateNewsPhotoTitle(int userProfileId, string oldTitle, string newTitle)
+        {
+            var success = true;
+            try
+            {
+                var query = context.NewsAndPhotos.Where(n => n.UserProfileId == userProfileId).Where(n => n.Title == oldTitle);
+                var result = query.First();
+                result.Title = newTitle;
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                success = false;                
+            }
+            return success;
+            
+        }
+
+        public NewsPhotoItem DeleteNewsPhotoItem(NewsPhotoItem news_item1)
+        {
+            var result = new NewsPhotoItem();
+            try
+            {
+                var query = context.NewsAndPhotos.Where(n => n == news_item1);
+                result = query.First();
+                context.NewsAndPhotos.Remove(result);
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }            
+            return result;
         }
     }
 }
