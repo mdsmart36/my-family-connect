dd678e8d08a7fcaff362fe80dc5fd597454e1aaf
Matt Smart <mdsmart36@gmail.com>
2015-12-02 12:54:08 -0600

    added app.js to handle open dropdown menu on hover event; included path in BundleConfig and script reference in _Layout.cshtml

---
+++ MyFamilyConnect/Scripts/app.js
@@ -0,0 +1,14 @@
+﻿// code snippet borrowed from http://bootsnipp.com/snippets/featured/show-dropdown-hover-amp-caret-up
+$(function () {
+    $(".dropdown").hover(
+            function () {
+                $('.dropdown-menu', this).stop(true, true).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('#caret', this).toggleClass("caret caret-up");
+            },
+            function () {
+                $('.dropdown-menu', this).stop(true, true).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('#caret', this).toggleClass("caret caret-up");
+            });
+});
